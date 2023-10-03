1cfb0e1e78f56b17809aa1cd3b76043c538db790
Justin Loh <justinlohkingwei@gmail.com>
2023-10-03 10:10:22 -0400

    Added Modal and Context Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Content from './components/Content';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { createContext, useState } from "react";
+import Modal from "./components/modals/Modals";
+
+export const GlobalContext = createContext<any>(null);
+
+export type globalContextTypes = {
+  showModal: boolean;
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+  currentModal: JSX.Element;
+  setCurrentModal: React.Dispatch<React.SetStateAction<JSX.Element>>;
+};
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [currentModal, setCurrentModal] = useState(<Modal />);
+
+  const globalContextValues: globalContextTypes = {
+    showModal,
+    setShowModal,
+    currentModal,
+    setCurrentModal,
+  };
+
   return (
-    <BrowserRouter>
-      <>
-        <Header />
-        <Content />
-        <Footer />
-      </>
-    </BrowserRouter>
+    <GlobalContext.Provider value={globalContextValues}>
+      <BrowserRouter>
+        <>
+          <Header />
+          <Content />
+          <Footer />
+        </>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   );
 }
 
